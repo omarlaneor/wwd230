82dbbbbfda254d4be214b68b903e69a97fd8e3c1
omarlaneor <annielaneor@gmail.com>
2023-01-11 09:40:06 -0500

    CodePen link and current date added

--- w01/scripts/script.js
+++ w01/scripts/script.js
@@ -6,3 +6,9 @@ function lastUpdated() {
     let a = document.lastModified;
     document.getElementById("lastmodified").innerHTML = a;
 }
+
+var f = new Date();
+        document.write(f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear());
+
+const options = {weekday: "long", day: "numeric", month: "long", year: "numeric"};
+document.getElementById("currentdate").textContent = new Date().toLocaleDateString("en-US", options);
